5cf8f5562a77c24a29fa37728d6c04163d2f3f68
Mohammad abir Abbas <mabbas@c2s14d73.42wolfsburg.de>
2022-08-25 18:55:07 +0200

    Rotate done. But now testing for print

--- stack_commands/rotate.c
+++ stack_commands/rotate.c
@@ -6,23 +6,64 @@
 /*   By: mabbas <mabbas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 18:16:14 by mabbas            #+#    #+#             */
-/*   Updated: 2022/08/22 21:20:41 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/08/25 18:54:15 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "instructions.h"
 
-void	insert(t_plst **head, t_plst *node)
-{
-	t_plst	**current;
 
-	current = head;
-	while (*current && (*curr)->data < node->data)
-	{
-		current = &((*current)->next);
-	}
-	node->next = *current;
-	*current = node;
+FVNRLS -->>> rmazurit !! 
+
+
+/**
+ * @brief Shift up all elements of stack a by 1
+ *        First element becomes the last one.
+ *        So created a new node and later 
+ *        added at the end to perform the
+ *        operation. We can try otherways too.
+ * 
+ * @param stack_a 
+ */
+void	ft_ra(t_list **stack_a)
+{
+	t_list		*tmp;
+
+	tmp = NULL;
+	if (!*stack)
+		return ;
+	tmp = ft_new_node((*stack_a)->input);
+	ft_back_add(stack_a, tmp);
+	tmp = *stack_a;
+	*stack = (*stack_a)->next;
+	free (tmp);
+	tmp = NULL;
+	write (1, "ra\n", 3);
+}
+
+void	ft_rb(t_list **stack_b)
+{
+	t_list	*tmp;
+
+	tmp = NULL;
+	if (!*stack_b)
+		return ;
+	tmp = ft_new_node((*stack_b)->input);
+	ft_back_add(stack_b, tmp);
+	tmp = *stack_b;
+	*stack = (*stack_b)->next;
+	free (tmp);
+	tmp = NULL;
+	write (1, "rb\n", 3);
+}
+
+void	ft_rr(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp;
+
+	ft_ra(stack_a);
+	ft_rb(stack_b);
+	write (1, "rr\n", 3);
 }
 
 void	print_list(t_plst *head)
@@ -33,10 +74,4 @@ void	print_list(t_plst *head)
 		head = head->next;
 	}
 	printf("NULL\n");
-}
-
-
-int main()
-{
-	
 }
